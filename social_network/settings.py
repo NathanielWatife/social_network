569b702cde8f0e50e8c384e8318a54b3667275d2
NathanielWatife <awosanminathaniel0@gmail.com>
2023-12-01 04:53:33 +0100

    configurations was made in models.py file

--- social_network/settings.py
+++ social_network/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media files for models and profile
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
